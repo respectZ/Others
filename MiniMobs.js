cd2643e7e8876b88644ac384a8aa616ed28b3825
respectZ <respectZ@users.noreply.github.com>
2015-07-03 18:16:25 +0700

    Update MiniMobs.js

--- MiniMobs.js
+++ MiniMobs.js
@@ -3,11 +3,13 @@ ModPE.setItem(1000,"spawn_egg",12,"Spawn Baby Zombie",1);
 ModPE.setItem(1001,"spawn_egg",13,"Spawn Baby Zombie Pigman",1);
 ModPE.setItem(1002,"spawn_egg",0,"Spawn Chicken Jockey",1);
 ModPE.setItem(1003,"spawn_egg",6,"Spawn Baby Creeper",1);
+ModPE.setItem(1004,"spawn_egg",9,"Spawn Baby Skeleton",1);
 var ready=true; 
 Item.setCategory(1000,3);
 Item.setCategory(1001,3);
 Item.setCategory(1002,3);
 Item.setCategory(1003,3);
+Item.setCategory(1004,3);
 
 function modTick() {
     if(ready) {
@@ -17,6 +19,7 @@ function modTick() {
             Player.addItemCreativeInv(1001,1);
             Player.addItemCreativeInv(1002,1);
             Player.addItemCreativeInv(1003,1);
+            Player.addItemCreativeInv(1004,1);
             Player.addItemCreativeInv(383,1,41);
         }
     }
@@ -46,7 +49,7 @@ function useItem(x,y,z,i,b,s) {
         if(rnd>10) {
             jock = Level.spawnMob(sides[s][0],sides[s][1],sides[s][2],33,"mob/babycreeper.png");
             Entity.setRenderType(jock,babycreeperRenderType.renderType);
-            Entity.setCollisionSize(jock,0.5,0.5);
+            Entity.setCollisionSize(jock,-1,-1);
         }
         var chicken = Level.spawnMob(sides[s][0],sides[s][1],sides[s][2],10);
         rideAnimal(jock,chicken);
@@ -54,7 +57,13 @@ function useItem(x,y,z,i,b,s) {
     if(i==1003) {
         var creepy = Level.spawnMob(sides[s][0],sides[s][1],sides[s][2],33,"mob/babycreeper.png");
         Entity.setRenderType(creepy,babycreeperRenderType.renderType);
-        Entity.setCollisionSize(creepy,0.5,0.5);
+        Entity.setCollisionSize(creepy,-1,-1);
+    }
+    if(i==1004) {
+        var skele = Leevl.spawnMob(sides[s][0],sides[s][1],sides[s][2],34,"mob/babyskeleton.png");
+        Entity.setAnimalAge(skele,-24000);
+        Entity.setCollisionSize(skele,-1,-1);
+        Entity.setRenderType(skele,babyskeletonRenderType.renderType);
     }
 }
 
@@ -86,3 +95,31 @@ body.addBox(-2,21,-4, 2, 3, 2);
 }
 var babycreeperRenderType = Renderer.createHumanoidRenderer();
 addbabycreeperRenderType(babycreeperRenderType);
+
+function addbabyskeletonRenderType(renderer){
+var model = renderer.getModel();
+var head = model.getPart("head").clear();
+var body = model.getPart("body").clear();
+var rArm = model.getPart("rightArm").clear();
+var lArm = model.getPart("leftArm").clear();
+var rLeg = model.getPart("rightLeg").clear();
+var lLeg = model.getPart("leftLeg").clear();
+body.setTextureOffset(0, 0, true);
+body.addBox(-2,11,-3, 4, 4, 4);
+body.setTextureOffset(8, 8, true);
+body.addBox(-2,15,-2, 4, 5, 2);
+body.setTextureOffset(0, 8, true);
+body.addBox(-3,15,-1.5, 1, 4, 1);
+body.setTextureOffset(0, 8, true);
+body.addBox(2,15,-1.5, 1, 4, 1);
+body.setTextureOffset(0, 8, true);
+body.addBox(-2,20,-1.5, 1, 4, 1);
+body.setTextureOffset(0, 8, true);
+body.addBox(1,20,-1.5, 1, 4, 1);
+}
+var babyskeletonRenderType = Renderer.createHumanoidRenderer();
+addbabyskeletonRenderType(babyskeletonRenderType);
+
+function leaveGame() {
+    ready=true;
+}
