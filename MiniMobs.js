7f958a2e56a73e38257be7ad7f9080a65687e71c
respectZ <respectZ@users.noreply.github.com>
2015-07-04 13:35:48 +0700

    Update MiniMobs.js

--- MiniMobs.js
+++ MiniMobs.js
@@ -1,5 +1,6 @@
 ModPE.overrideTexture("images/mob/babycreeper.png","https://dl.dropboxusercontent.com/s/uvwjqtxjflyp7s7/babycreeper.png?dl=0")
 ModPE.overrideTexture("images/mob/babyskeleton.png","https://dl.dropboxusercontent.com/s/xf2uoxadl8z36q3/babyskeleton.png?dl=0")
+ModPE.overrideTexture("images/mob/babyenderman.png","https://dl.dropboxusercontent.com/s/q9knz5ko0lwlcyu/babyenderman.png?dl=0")
 function addbabycreeperRenderType(renderer){
 var model = renderer.getModel();
 var head = model.getPart("head").clear();
@@ -47,6 +48,29 @@ body.addBox(1,20,-1.5, 1, 4, 1);
 }
 var babyskeletonRenderType = Renderer.createHumanoidRenderer();
 addbabyskeletonRenderType(babyskeletonRenderType);
+function addbabyendermanRenderType(renderer){
+var model = renderer.getModel();
+var head = model.getPart("head").clear();
+var body = model.getPart("body").clear();
+var rArm = model.getPart("rightArm").clear();
+var lArm = model.getPart("leftArm").clear();
+var rLeg = model.getPart("rightLeg").clear();
+var lLeg = model.getPart("leftLeg").clear();
+body.setTextureOffset( 0, 0,true);
+body.addBox(-2,7,-3, 5, 5, 5);
+body.setTextureOffset( 33, 23,true);
+body.addBox(-1,12,-2, 3, 6, 3);
+body.setTextureOffset( 56, 6,true);
+body.addBox(0,12,-3, 1, 8, 1);
+body.setTextureOffset( 58, 19,true);
+body.addBox(0,12,1, 1, 8, 1);
+body.setTextureOffset( 49, 22,true);
+body.addBox(0,18,-2, 1, 6, 1);
+body.setTextureOffset( 39, 21,true);
+body.addBox(0,18,0, 1, 6, 1);
+}
+var babyendermanRenderType = Renderer.createHumanoidRenderer();
+addbabyendermanRenderType(babyendermanRenderType);
 var spawner = [];
 setNewSpawner = function(id,tex,data,name) {
     ModPE.setItem(id,tex,data,"Spawn "+name,1);
@@ -55,6 +79,9 @@ setNewSpawner = function(id,tex,data,name) {
 }
 setNewSpawner(2100,"spawn_egg",6,"Baby Creeper");
 setNewSpawner(2101,"spawn_egg",9,"Baby Skeleton");
+setNewSpawner(2102,"spawn_egg",0,"Baby Creeper (Jockey)");
+setNewSpawner(2103,"spawn_egg",0,"Baby Skeleton (Jockey)");
+setNewSpawner(2104,"spawn_egg",7,"Baby Enderman");
 
 
 function useItem(x,y,z,i,b,s) {
@@ -72,6 +99,26 @@ function useItem(x,y,z,i,b,s) {
                     Entity.setRenderType(skele,babyskeletonRenderType.renderType);
                     Entity.setCarriedItem(skele,0,0,0);
                     Entity.setCollisionSize(skele,0.1,0.1);
+                }
+                if(i==2102) {
+                    var jockey = Level.spawnMob(sides[s][0]+0.5,sides[s][1],sides[s][2]+0.5,33,"mob/babycreeper.png");
+                    Entity.setRenderType(jockey,babycreeperRenderType.renderType);
+                    Entity.setCollisionSize(jockey,0.1,0.1);
+                    var chicken = Level.spawnMob(sides[s][0]+0.5,sides[s][1],sides[s][2]+0.5,10);
+                    rideAnimal(jockey,chicken)
+                }
+                if(i==2103) {
+                    var ske = Level.spawnMob(sides[s][0]+0.5,sides[s][1],sides[s][2]+0.5,34,"mob/babyskeleton.png");
+                    Entity.setRenderType(ske,babyskeletonRenderType.renderType);
+                    Entity.setCarriedItem(ske,0,0,0);
+                    Entity.setCollisionSize(ske,0.1,0.1);
+                    var chick = Level.spawnMob(sides[s][0]+0.5,sides[s][1],sides[s][2]+0.5,10);
+                    rideAnimal(ske,chick)
+                }
+                if(i==2104) {
+                    var men = Level.spawnMob(sides[s][0]+0.5,sides[s][1],sides[s][2]+0.5,38,"mob/babyenderman.png");
+                    Entity.setCollisionSize(men,0.1,0.1);
+                    Entity.setRenderType(men,babyendermanRenderType.renderType);
                 }
             } else {
                 if(i==2100) {
@@ -125,6 +172,11 @@ function checkMobs() {
             }
             if(Entity.getMobSkin(ent)=="mob/skeleton.png") {
                 Entity.setMobSkin(ent,"mob/babyskeleton.png");
+                Entity.setCollisionSize(ent,0.1,0.1);
+                Entity.setCarriedItem(ent,0,0,0);
+            }
+            if(Entity.getMobSkin(ent)=="mob/enderman.tga") {
+                Entity.setMobSkin(ent,"mob/babyenderman.png");
                 Entity.setCollisionSize(ent,0.1,0.1);
                 Entity.setCarriedItem(ent,0,0,0);
             }
